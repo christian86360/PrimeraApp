2ffcfac9be6291820ba825c05bd895443e04a38a
Christian Lugo <christian86360@gmail.com>
2021-11-05 19:19:41 -0600

    Se sube el contador

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import {Text,View} from 'react-native';
+import HolaMundoScreen from './src/screens/components/HolaMundoScreen';
+import ContadorScreen from './src/screens/ContadorScreen';
+
 
  const App = () => {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: 'red',
-      justifyContent: 'center'
-    }}>
-      <Text style={{
-        fontSize: 45,
-        textAlign: 'center'
-      }}>Hola Mundo</Text>
-    </View>
+    //<HolaMundoScreen/>
+    <ContadorScreen></ContadorScreen>
   )
 }
 
